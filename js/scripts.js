3e780414346b6bbb2a632a2cb428e32663a0b162
Andrew M Westling <a@andrewwestling.com>
2014-07-19 23:46:00 -0700

    Add logic; update tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,115 @@
 var splitter = function(rawInput) {
+	var numberArray = rawInput.toString().split("").map(Number);
+	var bigArray = [];
+	var originalLength = numberArray.length;
 
+	for (var i=0; i<originalLength; i+=3) {
+		bigArray.push(numberArray.slice(-3));
+		numberArray.splice(((numberArray.length)-3), 3);
+	};
+
+	return bigArray.reverse();
+
+	//returns format [[1],[2,3,4],[5,6,7],[8,9,0]]
+	//pass bigArray into numbersToWords();
+}
+
+
+var numbersToWords = function(rawInput) {
+
+	//first, split up the raw user input into the above format
+	var bigArray = splitter(rawInput);
+
+	var finalArray = [];
+	var powerNumber= 0;
+
+	var onesWords = { 	0: "", 
+											1: "one", 
+											2: "two", 
+											3: "three", 
+											4: "four", 
+											5: "five", 
+											6: "six", 
+											7: "seven", 
+											8: "eight", 
+											9: "nine" };
 	
-}
+	var teensWords = { 	10: "ten", 
+											11: "eleven", 
+											12: "twelve", 
+											13: "thirteen", 
+											14: "fourteen", 
+											15: "fifteen", 
+											16: "sixteen", 
+											17: "seventeen", 
+											18: "eighteen", 
+											19: "nineteen" };
 
-var numbersToWords = function(splitInput) {
+	var tensWords = { 	2: "twenty", 
+											3: "thirty", 
+											4: "forty", 
+											5: "fifty", 
+											6: "sixty", 
+											7: "seventy", 
+											8: "eighty", 
+											9: "ninety" };  
+	
+	var powerWords = {	1000: "thousand",
+											1000000: "million",
+											1000000000: "billion",
+											1000000000000: "trillion"};
 
-}
+	for (var i=0; i<bigArray.length; i++) {
+
+		var l = bigArray[i].length;
+		powerNumber = 0;
+
+		//special case: user inputs '0'
+		if (bigArray.length === 1 && bigArray[i][0] === 0) {
+			finalArray.push("zero");
+			break;
+		}
+
+		//evaluate hundreds digit
+		if (l === 3 && bigArray[i][0] !== 0) {
+			finalArray.push(onesWords[bigArray[i][0]] + " " + "hundred");
+		}
+
+		//evaluate tens and ones digits
+		//21 or greater
+		if (bigArray[i][l-2] >= 2 && bigArray[i][l-1] !== 0) {
+			finalArray.push(tensWords[bigArray[i][l-2]] + " " + onesWords[bigArray[i][l-1]]);
+		}
+		//20/30/40/.../90
+		else if (bigArray[i][l-2] >= 2 && bigArray[i][l-1] === 0) {
+			finalArray.push(tensWords[bigArray[i][l-2]]);
+		}
+		//11-19
+		else if (bigArray[i][l-2] === 1) {
+			var teenNumber = "1" + bigArray[i][l-1];
+			var teenNumberInt = parseInt(teenNumber);
+			finalArray.push(teensWords[teenNumberInt]);
+		}
+		//if the [l-2] index is a zero (i.e. '0' in 201)
+		else if (bigArray[i][l-2] === 0) {
+			//and if the [l-1] index is NOT a zero (i.e. '1' in 201)
+			if (bigArray[i][l-1] !== 0) {
+				finalArray.push(onesWords[bigArray[i][l-1]]);
+			}
+		//single digit in bigArray[i] (i.e. 1)
+		} else if (l = 1 && bigArray[i][0] !== 0) {
+			finalArray.push(onesWords[bigArray[i][l-1]]);
+		}
+
+		//thousands/millions/billions/trillions
+		powerNumber = Math.pow(10,((bigArray.length-i-1) * 3));
+		if (powerNumber >= 1000 && (bigArray[i][0] !== 0 || bigArray[i][1] !== 0 || bigArray[i][2] !== 0)) {
+			finalArray.push(powerWords[powerNumber]);
+		}
+
+	} //end of the for loop
+
+	return finalArray.join(" ");
+
+
+} //end function
--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,67 @@ describe("splitter", function() {
 	it("turns the input '1' into [[1]]", function() {
 		splitter("1").should.eql([[1]]);
 	});
+	it("turns the input '123' into [[1,2,3]]", function() {
+		splitter("123").should.eql([[1,2,3]]);
+	});
+	it("turns the input '1234' into [[1],[2,3,4]]", function() {
+		splitter("1234").should.eql([[1],[2,3,4]]);
+	});
+	it("turns the input '1234567' into [[1],[2,3,4],[5,6,7]]", function() {
+		splitter("1234").should.eql([[1],[2,3,4]]);
+	});
+	it("turns the input '1234567890' into [[1],[2,3,4],[5,6,7],[8,9,0]]", function() {
+		splitter("1234").should.eql([[1],[2,3,4]]);
+	});
 });
 
 describe("numbersToWords", function() {
-	it("returns 'zero' if the user enters 0", function() {
+	it("returns 'zero' if the user enters 0 (hard-coded zero validation works)", function() {
 		numbersToWords(0).should.equal("zero");
+	});
+	it("returns 'one' if the user enters 1 (1-9 work)", function() {
+		numbersToWords(1).should.equal("one");
+	});
+	it("returns 'eleven' if the user enters 11 (11-19 work)", function() {
+		numbersToWords(11).should.equal("eleven");
+	});
+	it("returns 'twenty' if the user enters 20 (20/30/40/.../90 work)", function() {
+		numbersToWords(20).should.equal("twenty");
+	});
+	it("returns 'twenty one' if the user enters 21 (tensWords + onesWords works)", function() {
+		numbersToWords(21).should.equal("twenty one");
+	});
+	it("returns 'one hundred' if the user enters 100 (means 0's in [l-2] and [l-1] are disregarded)", function() {
+		numbersToWords(100).should.equal("one hundred");
+	});
+	it("returns 'one hundred one' if the user enters 101 (means nonzero numbers in [l-1] following a 0 in [l-2] are still printed in output)", function() {
+		numbersToWords(101).should.equal("one hundred one");
+	});
+	it("returns 'one hundred eleven' if the user enters 111 (111-119 work)", function() {
+		numbersToWords(111).should.equal("one hundred eleven");
+	});
+	it("returns 'one hundred twenty' if the user enters 120 (120-190 work)", function() {
+		numbersToWords(120).should.equal("one hundred twenty");
+	});
+	it("returns 'one hundred twenty one' if the user enters 121 (121-199 work)", function() {
+		numbersToWords(121).should.equal("one hundred twenty one");
+	});
+	it("returns 'one thousand' if the user enters 1000 (thousand word is printed; zeros are disregarded)", function() {
+		numbersToWords(1000).should.equal("one thousand");
+	});
+	it("returns 'eleven thousand' if the user enters 11000 (bigArray[i]'s with [0,0,0] don't get printed)", function() {
+		numbersToWords(11000).should.equal("eleven thousand");
+	});
+	it("returns 'one thousand one' if the user enters 1001 (middle zeroes are evaluated properly after a powerWord)", function() {
+		numbersToWords(1001).should.equal("one thousand one");
+	});
+	it("returns 'one million' if the user enters 1000000 (million word is printed; zeroes are disregarded)", function() {
+		numbersToWords(1000000).should.equal("one million");
+	});
+	it("returns 'one billion' if the user enters 1000000000 (billion word is printed; zeroes are disregarded)", function() {
+		numbersToWords(1000000000).should.equal("one billion");
+	});
+	it("returns 'one trillion' if the user enters 1000000000000 (trillion word is printed; zeroes are disregarded)", function() {
+		numbersToWords(1000000000000).should.equal("one trillion");
 	});
 });
